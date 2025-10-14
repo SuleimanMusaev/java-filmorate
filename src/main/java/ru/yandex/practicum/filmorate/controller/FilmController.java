44506f8055b5101b46558bed945add06fc76e8a5
Sula M <SuleimanMusa@yandex.com>
2025-10-14 20:09:02 +0200

    исправлены порядки модификаторов

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -14,8 +14,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("/films")
 public class FilmController {
-    private final static Logger log = LoggerFactory.getLogger(FilmController.class);
-    private final static int MAX_LENGTH_OF_DESCRIPTION = 200;
+    private static final Logger log = LoggerFactory.getLogger(FilmController.class);
+    private static final int MAX_LENGTH_OF_DESCRIPTION = 200;
     private final Map<Long, Film> films = new HashMap<>();
 
     @GetMapping

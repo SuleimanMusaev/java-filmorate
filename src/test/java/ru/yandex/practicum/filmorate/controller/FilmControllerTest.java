e0a754243e24d3065bf2c494ce544050904cbf8c
Sula M <SuleimanMusa@yandex.com>
2025-10-28 21:19:18 +0100

    Доработка эндпоинтов и REST-API. Добавление хранения данных

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,15 +1,15 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.model.Film;
-
-import java.lang.reflect.Method;
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import ru.yandex.practicum.filmorate.exception.ValidationException;
+//import ru.yandex.practicum.filmorate.model.Film;
+//
+//import java.lang.reflect.Method;
+//import java.time.LocalDate;
+//
+//import static org.junit.jupiter.api.Assertions.*;
 
 class FilmControllerTest {
 //    private FilmController controller;
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,15 +1,15 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.model.User;
-
-import java.lang.reflect.Method;
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import ru.yandex.practicum.filmorate.exception.ValidationException;
+//import ru.yandex.practicum.filmorate.model.User;
+//
+//import java.lang.reflect.Method;
+//import java.time.LocalDate;
+//
+//import static org.junit.jupiter.api.Assertions.*;
 
 class UserControllerTest {
 //    private UserController controller;

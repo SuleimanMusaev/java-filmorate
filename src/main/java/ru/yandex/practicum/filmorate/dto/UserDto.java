6edb3c2056d76332f5cda9175dcde8b8f62cbc9a
BelovEvgenii <Zhekab777@yandex.ru>
2026-01-22 00:45:37 +0300

    Merge remote-tracking branch 'origin/add-director' into add-director

--- src/main/java/ru/yandex/practicum/filmorate/dto/UserDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/UserDto.java
@@ -2,8 +2,13 @@ package ru.yandex.practicum.filmorate.dto;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class UserDto {
     Long id;
-
+    String email;
+    String login;
+    String name;
+    LocalDate birthday;
 }

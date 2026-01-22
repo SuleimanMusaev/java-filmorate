5251ef1e89c5eb24b225797a32ae084436e606a5
Suleiman Musaev <suli141198@gmail.com>
2026-01-22 20:03:02 +0100

    добавлен функционал -лента событий-

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/EventController.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Event;
+import ru.yandex.practicum.filmorate.service.EventService;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+//@RequestMapping("/events")
+public class EventController {
+    private final EventService service;
+
+    @GetMapping("/users/{userId}/feed")
+    public List<Event> getFeed(@PathVariable Long userId) {
+        return service.getUserFeed(userId);
+    }
+}

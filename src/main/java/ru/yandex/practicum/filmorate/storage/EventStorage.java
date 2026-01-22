ad67dbb9d8d269fdfdb875795c34b804db152707
Suleiman Musaev <suli141198@gmail.com>
2026-01-22 20:13:25 +0100

    добавлен функционал -лента событий-

--- src/main/java/ru/yandex/practicum/filmorate/storage/EventStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/EventStorage.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public interface EventStorage {
     void addEvent(Long userId, Long entityId, String eventType, String operation);
+
     List<Event> getUserFeed(Long userId);
 }
